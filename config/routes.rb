e06424400a2c3b228dc2e9b1eec1d9ca5278f8d9
NGUYEN VAN DUNG <dungnv4989@gmail.com>
2017-12-04 18:34:20 -0800

    load app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :microposts
+  	resources :microposts
     resources :users
-    root 'users#index'
+    root 'UsersController#index'
   #root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
